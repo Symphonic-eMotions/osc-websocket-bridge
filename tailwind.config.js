95960f0a3e6a97b2f1d584ec67a2f43c8668d925
Ubuntu Juan <ubuntu-juan@yourule.nl>
2024-11-19 00:42:50 +0100

    Start server from console
    css als output.css gerenderd

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./*.html", // Zorg ervoor dat dit pad naar jouw HTML-bestanden verwijst
+    "./src/**/*.{js,ts,jsx,tsx}", // Voeg hier extra paden toe voor JavaScript-bestanden als nodig
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
