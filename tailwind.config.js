5064d8f45efaeb99b981e252896b806ce8e9b7a8
Symphonic eMotions <info@symphonic-emotions.nl>
2024-11-23 14:30:55 +0100

    tailwind config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@ module.exports = {
   content: [
     "./*.html", // Zorg ervoor dat dit pad naar jouw HTML-bestanden verwijst
     "./src/**/*.{js,ts,jsx,tsx}", // Voeg hier extra paden toe voor JavaScript-bestanden als nodig
+    './src/server/views/**/*.ejs',
+    './src/public/pages/**/*.html',
   ],
   theme: {
     extend: {},
